2ca3e5891c0404742a7dd2c3be163d1d550a62a6
Martin Safer <mtsafer@gmail.com>
2016-06-01 15:10:18 -0700

    Add comments to problems 1-2 in ruby. Improve problem-1 in ruby

--- problem-1.rb
+++ problem-1.rb
@@ -1,7 +1,10 @@
 multiples = []
 max = 1000
 
-def find_multiples number, max, multiples
+#returns an array of all of the multiples of the given number
+#where all of the multiples are less than the given max.
+#can take an optional array as a third argument to input multiples into.
+def find_multiples (number, max, multiples = [])
 	temp = number
 	while number < max do
 		multiples<<number unless multiples.include? number
@@ -10,13 +13,14 @@ def find_multiples number, max, multiples
 	multiples
 end
 
+#returns the sum of the elements in the given array.
 def sum_array array
 	sum = 0
 	array.each {|n| sum += n}
 	sum
 end
 
-multiples = find_multiples(3, max, [])
+multiples = find_multiples(3, max)
 multiples = find_multiples(5, max, multiples)
 
 puts sum_array multiples
--- problem-2.rb
+++ problem-2.rb
@@ -1,5 +1,7 @@
 max = 4000000
 
+#returns an array of the fibonacci sequence where the
+#final digit is less than or equal to the given max
 def generate_fibonacci max
 	fib = [1,2,3]
 	while fib[-1] <= max do
@@ -8,12 +10,14 @@ def generate_fibonacci max
 	fib
 end
 
+#returns an array of the even elements from the given array
 def find_evens array
 	evens = []
 	array.each {|num| evens<<num if num % 2 == 0}
 	evens
 end
 
+#returns the sum of the elements in the given array
 def sum_array array
 	sum = 0
 	array.each {|n| sum += n}
